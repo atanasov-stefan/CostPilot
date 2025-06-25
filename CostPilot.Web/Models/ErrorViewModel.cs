7bf36c6e283c83a6061b1aa0e1fafd3809ad557a
atanasov-stefan <s.atanasov0sa@gmail.com>
2025-06-25 19:15:27 +0300

    Created Web Project

---
+++ CostPilot.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CostPilot.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
