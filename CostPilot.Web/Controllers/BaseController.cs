98d81aa6b273f7ebf2aee8d44f895fc4cefa10e4
atanasov-stefan <s.atanasov0sa@gmail.com>
2025-06-29 14:40:57 +0300

    Created Base Controller, CostCenter Controller, CostCenter Index View

---
+++ CostPilot.Web/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CostPilot.Web.Controllers
+{
+    [Authorize]
+    public abstract class BaseController : Controller
+    {
+    }
+}
---
+++ CostPilot.Web/Controllers/CostCenterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CostPilot.Web.Controllers
+{
+    public class CostCenterController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
